06c13473691c3e20ce2d76bdc116ef27e7e6aae9
deusmemes <maxin29999@gmail.com>
2019-06-01 04:33:47 +0400

    начат личный кабинет

--- KorytoServiceImplementDataBase/Implementations/StatisticServiceDB.cs
+++ KorytoServiceImplementDataBase/Implementations/StatisticServiceDB.cs
@@ -32,5 +32,18 @@ namespace KorytoServiceImplementDataBase.Implementations
 
             return name + " в количестве " + count + " машин";
         }
+
+        private string GetCars(int clientId)
+        {
+            var clientCars = context.OrderCars.Where(rec => rec.Order.ClientId == clientId).Select(rec => rec);
+
+            var most = clientCars
+                .GroupBy(rec => rec.CarId)
+                .Select(rec => new { Id = rec.Key, Total = rec.Sum(x => x.Amount)})
+                .OrderByDescending(rec => rec.Total)
+                .First();
+
+            return "";
+        }
     }
 }
